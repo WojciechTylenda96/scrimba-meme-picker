5328a803431e2c5bca1e4f540512893f023463b6
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2025-01-20 17:56:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById("get-image-btn")
+const gifsOnlyOption = document.getElementById("gifs-only-option")
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 getImageBtn.addEventListener('click', getMatchingCatsArray)
@@ -16,11 +17,19 @@ function highlightCheckedOption(e){
     document.getElementById(e.target.id).parentElement.classList.add("highlight")
 }
 
-// pokazuje w konsoli wartość wybranego pola (emocja)
+// pokazuje w konsoli wartość wybranego pola (emocja) oraz czy zaznaczono opcję "GIFs only"
 function getMatchingCatsArray(){
     if(document.querySelector("input[type='radio']:checked")){
         const checkedEmotion = document.querySelector("input[type='radio']:checked").value
         console.log(checkedEmotion)
+    }
+
+    if(document.querySelector("input[type='checkbox']:checked")){
+        const isGifOnly = true
+        console.log(isGifOnly)
+    } else {
+        const isGifOnly = false
+        console.log(isGifOnly)
     }
 }
 
